cf1c6ce3aa472cb6bdfc3c43db033aeab5b5a3e0
SANUJA GUPTA <sanujagupta340@gmail.com>
2026-04-29 17:25:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def generate():
     """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
